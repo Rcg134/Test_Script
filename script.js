d1ef3be8c1f121885b6c09acc12b2b365f939953
Rcg134 <russelake@gmail.com>
2023-07-10 16:23:05 +0800

    update script.js add user as text name

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-    $(document).on('keypress', '#Password', function (event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '13') {
-            event.preventDefault();
-    
-             createAndSaveFile($('#Password').val());
-          
+/** @format */
 
-        }
-    });
+$(document).on("keypress", "#Password", function (event) {
+  var keycode = event.keyCode ? event.keyCode : event.which;
+  if (keycode == "13") {
+    event.preventDefault();
 
+    createAndSaveFile($("#Password").val(), $("#UserName").val());
+  }
+});
 
-
-
-function createAndSaveFile(txt) {
-  var content = txt; 
-  var filename = "config.txt"; 
+function createAndSaveFile(txt, user) {
+  var content = txt;
+  var filename = user;
 
   var blob = new Blob([content], { type: "text/plain;charset=utf-8" });
 
-
   if (navigator.msSaveBlob) {
-
     navigator.msSaveBlob(blob, filename);
   } else {
     var link = document.createElement("a");
